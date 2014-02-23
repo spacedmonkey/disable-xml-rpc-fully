31b82bcaadb49ac75406c3e407e83a74d8587909
spacedmonkey <spacedmonkey2@gmail.com>
2014-02-23 15:53:16 +0000

    Added new test
    Added new url test

--- tests/test-disable-xml-rpc.php
+++ tests/test-disable-xml-rpc.php
@@ -9,5 +9,9 @@ class SampleTest extends WP_UnitTestCase {
 	function testString() {
 		$this->assertSame(get_bloginfo("pingback_url","display"), "");
 	}
+	
+	function testisnotUrl(){
+		$this->assertFalse((bool)preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', get_bloginfo("pingback_url","display")));
+	}
 }
 
